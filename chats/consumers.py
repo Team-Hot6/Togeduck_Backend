a181cdf5866a8a3004511febb35204b53e6d6875
Carrotww <rjgjfl90@naver.com>
2022-12-02 17:43:53 +0900

    update consumers.py add disconnect, receive test done

--- chats/consumers.py
+++ chats/consumers.py
@@ -5,10 +5,34 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
         
-        await self.send(text_data=json.dumps({
-            'type':'connect hyeong',
-            'message':'plz suc'
-            }))
+        # send message example
+        # await self.send(text_data=json.dumps({
+        #     'type':'connect hyeong',
+        #     'message':'plz suc'
+        #     }))
+    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+    # get message receive example
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        msg = text_data_json['message']
+        print(msg)
+
+
+class CreateRoom(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_id"]
+
+        await self.accept()
+    
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        msg = text_data_json['message']
 
 # class ChatConsumer(AsyncWebsocketConsumer):
 #     async def connect(self):
