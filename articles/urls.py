0e64db5adfdde76c20b16296f0a237f75d0e750d
9yuhyeon <kkh2742@gmail.com>
2022-12-02 16:03:49 +0900

    create : 커뮤니티 게시글 상세 페이지 조회 기능 추가

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,5 @@ from articles import views
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='article_view'),
     path('create/', views.ArticleCreateView.as_view(), name='article_create_view'),
+    path('<int:article_id>/', views.ArticleDetailView.as_view(), name='article_detail_view'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,7 @@ from articles.models import Article
 from articles.serializers import ArticleSerializer
 from rest_framework.response import Response
 from rest_framework import status, permissions
+from rest_framework.generics import get_object_or_404
 
 
 # 게시글 전체 보기
@@ -12,9 +13,9 @@ class ArticleView(APIView):
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-# 게시글 생성
+
+# 게시글 작성페이지
 class ArticleCreateView(APIView):
-    # 게시글 작성
     def post(self, request):
         if not request.user.is_authenticated:
             return Response({"msg":"로그인이 필요합니다!"}, status=status.HTTP_401_UNAUTHORIZED)
@@ -24,3 +25,10 @@ class ArticleCreateView(APIView):
             serialzier.save(user=request.user)
             return Response(serialzier.data, status=status.HTTP_201_CREATED)
         return Response(serialzier.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# 게시글 상세페이지
+class ArticleDetailView(APIView):
+    def get(self, request, article_id):
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_200_OK)
