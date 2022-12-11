477d4824aa5a66b694cea4cae7e30cb3b3439089
Carrotww <rjgjfl90@naver.com>
2022-12-12 01:42:45 +0900

    update : 로직 수정, time score 남음

--- articles/articlecron.py
+++ articles/articlecron.py
@@ -4,14 +4,18 @@ from django.db.models import Q
 
 if __name__ == "__main__":
     category_score_dict = {1: 20, 2: 20, 3: 20, 4: 20, 5: 20, 6: 20, 7: 20, 8: 20, 9: 20}
+    like_multiple = 1.2
+    view_multiple = 1
+    time_multiple = 1
     
-    def score(obj) -> int:
-        result = 0
-        result += category_score_dict[obj.category]
-        result += 
-        
-        return 
     # 취미 카테고리별 점수
+    def score(obj) -> int:
+        result = 1
+        result *= category_score_dict[obj.category]
+        result *= obj.like * like_multiple
+        result *= obj.views * view_multiple
+        
+        return result * time_multiple
 
     time_now = datetime.now()
 
@@ -20,5 +24,10 @@ if __name__ == "__main__":
         &
         Q(update_at__gte=datetime.now() - timedelta(days=2)))
     
-    article_temp_list = []
-    
+    article_score_id_list = [(x, score(x)) for x in article_obj]
+
+    article_score_id_list.sort(key=lambda x:x[1], reverse=True)
+
+    result_id = [article_score_id_list[x][0] for x in range(len(article_score_id_list))]
+
+    print(result_id)
