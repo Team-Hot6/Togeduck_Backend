18819b6d3a9d35c0b8d1258758d7c38767ca3527
sunmi-park <ppssmm0331@gmail.com>
2022-12-14 18:45:41 +0900

    지역 추가 풀 떙기기

--- users/serializers.py
+++ users/serializers.py
@@ -109,7 +109,8 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        token['email'] = user.email 
+        token['email'] = user.email
+        token['nickname'] = user.nickname  
 
         # 채팅에서 쓸 user id 저장해줌
         token['user_id'] = user.id
