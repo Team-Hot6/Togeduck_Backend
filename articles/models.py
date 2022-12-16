0af5d9da69061160d3f2246618cbfc7e4475e39f
9yuhyeon <kkh2742@gmail.com>
2022-12-17 07:15:35 +0900

    update : Article 모델 article_image 필드 default 이미지 추가(Togeduck LOGO)
    - 게시글 작성 시 이미지 첨부를 하지 않을 경우 default로 Togeduck
    이미지 적용

--- articles/models.py
+++ articles/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     category = models.ForeignKey(Hobby, on_delete=models.CASCADE, related_name='article_category')
     title = models.CharField(max_length=500)
     content = models.TextField()
-    article_image = models.ImageField(upload_to=rename_imagefile_to_uuid, blank=True, null=True)
+    article_image = models.ImageField(upload_to=rename_imagefile_to_uuid, default='article/together_logo.PNG', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     like = models.ManyToManyField(User, blank=True, related_name='article_like')
