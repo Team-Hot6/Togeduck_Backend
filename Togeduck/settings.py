2406b83685e484dca5074f0f7a446cd22a83e4f2
Carrotww <rjgjfl90@naver.com>
2022-12-20 22:13:14 +0900

    create settings redis 추가

--- Togeduck/settings.py
+++ Togeduck/settings.py
@@ -77,10 +77,14 @@ ASGI_APPLICATION = "Togeduck.asgi.application"
 WSGI_APPLICATION = "Togeduck.wsgi.application"
 
 CHANNEL_LAYERS = {
-    'default' : {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
-    }
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+        },
+    },
 }
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -159,17 +163,6 @@ TEMPLATES = [
         },
     },
 ]
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
