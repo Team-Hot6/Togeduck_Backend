baee54c99c0435e11515c8964a73518f3ddd3817
sunmi-park <ppssmm0331@gmail.com>
2022-12-22 02:18:47 +0900

    메인 풀때기기전에 커밋

--- Togeduck/settings.py
+++ Togeduck/settings.py
@@ -163,7 +163,14 @@ TEMPLATES = [
         },
     },
 ]
+# settings.py 166번줄에 추가
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
