103bcb12d0cf26ca9aaffbc1a475cb6212324afe
9yuhyeon <kkh2742@gmail.com>
2022-12-21 22:26:45 +0900

    temp : 카카오 소셜 로그인 작업중

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from rest_framework.generics import get_object_or_404
 from users.models import User
 from users.serializers import CustomTokenObtainPairSerializer, UserSerializer, ChangePasswordSerializer, MypageInfoPutSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import (
     TokenObtainPairView   
 )
@@ -66,3 +67,91 @@ class MypageInfoPutView(APIView):
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"msg":"권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+import requests
+# STEP 2 : 전달 받은 코드 값과 함께 카카오 서버에 토큰 요청
+class KakaoCallBackView(APIView):
+    def get(self, request):
+        code = request.GET.get('code')
+        print('전달받은 코드 값 : ', code)
+        
+        if not code:
+            error = request.GET.get('error')
+            print("에러 정보 확인 테스트: " ,error)
+            error_description = request.GET.get('error_description')
+            return Response({"message": error_description}, status=status.HTTP_400_BAD_REQUEST)
+        
+        rest_api_key = "aea6c7b586cd433bf7056d8ab7cd1862",
+        redirect_uri = "http://127.0.0.1:8000/users/kakao/callback/"
+
+        data = {
+            "grant_type": "authorization_code",
+            "client_id": rest_api_key,
+            "redirect_uri": redirect_uri,
+            "code": code,
+        }
+
+        kakao_token_api = 'https://kauth.kakao.com/oauth/token'
+        
+        # 문제 : 전달 받은 코드와 필수 파라미터 값을 카카오 서버에 post 요청 보내야 하는데 TypeError: 'QueryDict' object is not callable 에러 발생!
+        # 원인 : request는 dictioinoary type(QueryDict)으로 변환하는데 key 값을 가진 key가 존재하면 해당 key에 대한 값을 반환하고, 존재하지 않을 경우 KeyError가 발생(요청 보낼 주소는 dict 타입이 아님)
+        # 해결방법 : python의 requests 모듈을 사용하여 post 요청하는 방법
+        request_token = requests.post(kakao_token_api, data=data).json()
+
+        # STEP 3 : 코드를 사용하여 전달받은 토큰을 헤더에 실어 아래 kakao_user_api로 GET 요청하여 토큰에 담긴 사용자 정보 가져온다. 
+        kakao_access_token = request_token['access_token']
+        kakao_refresh_token = request_token['refresh_token']
+        
+        print("카카오 엑세스 토큰 : " , kakao_access_token)
+        print("카카오 리프레쉬 토큰 : " , kakao_refresh_token)
+        
+        kakao_user_api = 'https://kapi.kakao.com/v2/user/me'
+        header = {"Authorization": f"Bearer ${kakao_access_token}"}
+        user_info = requests.get(kakao_user_api, headers=header).json()
+
+        # 유저 정보 response ex)
+        # {'id': 2588628171, 'connected_at': '2022-12-21T00:12:25Z', 
+        # 'properties': {'nickname': '규현', 'profile_image': 'http://k.kakaocdn.net/dn/bBDjw3/btrJSLREbyo/klPh6YQud6Exb8OYGB1Jr0/img_640x640.jpg', 'thumbnail_image': 'http://k.kakaocdn.net/dn/bBDjw3/btrJSLREbyo/klPh6YQud6Exb8OYGB1Jr0/img_110x110.jpg'}, 
+        # 'kakao_account': {'profile_nickname_needs_agreement': False, 'profile_image_needs_agreement': False, 
+        # 'profile': {'nickname': '규현', 'thumbnail_image_url': 'http://k.kakaocdn.net/dn/bBDjw3/btrJSLREbyo/klPh6YQud6Exb8OYGB1Jr0/img_110x110.jpg', 'profile_image_url': 'http://k.kakaocdn.net/dn/bBDjw3/btrJSLREbyo/klPh6YQud6Exb8OYGB1Jr0/img_640x640.jpg', 'is_default_image': False},
+        # 'has_email': True, 'email_needs_agreement': False, 'is_email_valid': True, 'is_email_verified': True, 'email': 'rlarbgus95@naver.com'}}
+        
+        user_email_agreement = user_info['kakao_account']['email_needs_agreement']
+        # 디버깅용 True
+        user_email_agreement = bool(True)
+        # 이메일 제공 동의
+        if user_email_agreement == True:
+            has_email = user_info['kakao_account']['has_email']
+            # 이메일 존재 여부
+            if has_email == True:
+                is_email_valid = user_info['kakao_account']['is_email_valid']
+                # 이메일 유효 여부
+                if is_email_valid == True:
+                    is_email_verified = user_info['kakao_account']['is_email_verified']
+                    # 이메일 인증 여부
+                    if is_email_verified == True:
+                        # 유저 이메일 정보
+                        email = user_info['kakao_account']['email']
+        
+        user_profile_image_agreement = user_info['kakao_account']['profile_image_needs_agreement']
+        # 디버깅용 True
+        user_profile_image_agreement = bool(True)
+        # 프로필 사진 제공 동의
+        if user_profile_image_agreement == True:
+            # 유저 프로필 정보
+            user_profile = user_info['kakao_account']['profile']['profile_image_url']
+        # 유저 닉네임 정보
+        user_nickname = user_info['kakao_account']['profile']['nickname']
+
+        exist_user = User.objects.filter(nickname=user_nickname)
+        
+        # 가입된 유저는 로그인
+        if exist_user:
+            user = User.objects.get(email=email)
+
+            pass
+        # 가입되지 않은 유저는 회원가입
+        else:
+            
+            pass
+        pass
